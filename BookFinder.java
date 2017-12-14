6bfe06a08defa5cd4a97628ab77e0c529e29ab76
Violet <violetpfeiffer@gmail.com>
2017-12-13 23:29:33 -0500

    Add list by genre

--- BookFinder.java
+++ BookFinder.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 import java.sql.*;
 
 public class BookFinder {
-    static Connection database;
+    static Scanner reader; //for user input
+    static Connection database; //for database wrapper
 
     public static void main(String[] args) {        
-        Scanner reader = new Scanner(System.in);
+        reader = new Scanner(System.in);
 
         // connect to SQL database
         try{
@@ -29,6 +30,7 @@ public class BookFinder {
             System.out.println("0: Exit");
 
             int choice = reader.nextInt();
+            reader.nextLine(); //flush scanner
 
             // exit program
             if (choice == 0){
@@ -54,7 +56,9 @@ public class BookFinder {
         reader.close();
     }
 
+    //print all authors
     private static void listAuthors(){
+        // print all authors
         try {
             Statement statement = database.createStatement();
             ResultSet rs = statement.executeQuery( "SELECT name FROM AUTHOR;");
@@ -69,8 +73,28 @@ public class BookFinder {
         }
     }
 
+    // List genre chosen by user
     private static void listByGenre(){
-        // To do
+        // get genre from user
+        System.out.println("Select a genre: Fantasy, Fiction, Nonfiction");
+        String genre = reader.nextLine();
+
+        // print all books in genre
+        try {
+            PreparedStatement statement = database.prepareStatement("SELECT * FROM BOOK WHERE GENRE = ?");
+            statement.setString(1, genre);
+            // get selected genre from database
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()){
+                // Column
+                String  title = rs.getString("title");
+                System.out.println(String.format("Title: %s, Author: %s, Publisher: %s, Genre: %s, Rating: %s, Series: %s", rs.getString("title"), "", "",rs.getString("genre"), rs.getString("rating"),rs.getString("series") ));
+            }
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
     }
 
     private static void listByAuthor(){
