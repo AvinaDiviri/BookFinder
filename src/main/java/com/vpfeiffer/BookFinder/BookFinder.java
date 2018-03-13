d74c6572c3836ab5f253b736ffc301dd86468638
Violet Pfeiffer <vpfeiffer.git@gmail.com>
2018-03-13 05:17:31 -0400

    Remove unnecessary white spaces

--- src/main/java/com/vpfeiffer/BookFinder/BookFinder.java
+++ src/main/java/com/vpfeiffer/BookFinder/BookFinder.java
@@ -20,24 +20,25 @@ public final class BookFinder {
 
     /* Utility class for accessing database
     */
-    private BookFinder() { 
+    private BookFinder() {
     }
 
     public static void main(String[] args) {
         reader = new Scanner(System.in);
 
         // connect to SQL database
-        try{
+        try {
             Class.forName("org.sqlite.JDBC");
             database = DriverManager.getConnection("jdbc:sqlite:books.db");
-            database.setAutoCommit(false); // prevents database from being modified
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            // Prevent database from being edited.
+            database.setAutoCommit(false);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
 
         // main UI loop
-        while(true){
+        while (true) {
 
             // UI options
             System.out.println("1: List authors");
@@ -49,12 +50,12 @@ public final class BookFinder {
             reader.nextLine(); //flush scanner
 
             // exit program
-            if (choice == 0){
+            if (choice == 0) {
                 break;
             }
 
             // UI options logic
-            switch(choice){
+            switch (choice) {
                 case 1:
                     listAuthors();
                     break;
@@ -73,24 +74,24 @@ public final class BookFinder {
     }
 
     //print all authors
-    private static void listAuthors(){
+    private static void listAuthors() {
         // print all authors
         try {
             Statement statement = database.createStatement();
-            ResultSet rs = statement.executeQuery( "SELECT name FROM AUTHOR;");
+            ResultSet rs = statement.executeQuery("SELECT name FROM AUTHOR;");
 
-            while (rs.next()){
+            while (rs.next()) {
                 String  name = rs.getString("name");
                 System.out.println(name);
             }
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
 
     // List genre chosen by user
-    private static void listByGenre(){
+    private static void listByGenre() {
         // get genre from user
         System.out.println("Select a genre: Fantasy, Fiction, Nonfiction");
         String genre = reader.nextLine();
@@ -102,18 +103,18 @@ public final class BookFinder {
             // get selected genre from database
             ResultSet rs = statement.executeQuery();
 
-            while (rs.next()){
+            while (rs.next()) {
                 // Column
                 String  title = rs.getString("title");
-                System.out.println(String.format("Title: %s, Author: %s, Publisher: %s, Genre: %s, Rating: %s, Series: %s", rs.getString(1), rs.getString(2), rs.getString(3),rs.getString(4), rs.getString(5),rs.getString(6) ));
+                System.out.println(String.format("Title: %s, Author: %s, Publisher: %s, Genre: %s, Rating: %s, Series: %s", rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6)));
             }
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
 
-    private static void listByAuthor(){
+    private static void listByAuthor() {
         // To do
     }
 
