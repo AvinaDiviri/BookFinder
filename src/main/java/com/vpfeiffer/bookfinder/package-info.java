41bc0882560c4b537a0aaa30bd0d7894932327d2
Violet Pfeiffer <vpfeiffer.git@gmail.com>
2018-03-13 05:42:40 -0400

    Create package info file

---
+++ src/main/java/com/vpfeiffer/bookfinder/package-info.java
@@ -0,0 +1,12 @@
+/**
+* Domain classes used to produce command line user interface for database.
+* <p>
+* This class contains the controller and UI.
+* </p>
+*
+* @since 1.0
+* @author Violet Pfeiffer
+* @version 1.0
+*/
+
+package main.java.com.vpfeiffer.bookfinder;
